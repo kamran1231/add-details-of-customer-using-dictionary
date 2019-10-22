3ffafad4d684e64383c1b376c3aa164221601bed
kamran khan <48378125+kamran1231@users.noreply.github.com>
2019-10-22 19:32:55 +0530

    Add files via upload

---
+++ add_dict.py
@@ -0,0 +1,18 @@
+
+def add_cust(id,name,age,fav_movies):
+    cus_dict['id'] = id
+    cus_dict['name'] = name
+    cus_dict['age'] = age
+    cus_dict['fav_movies'] = fav_movies
+
+    for key,value in cus_dict.items():
+        print(f'{key} : {value}')
+
+
+
+id = int(input('enter the customer id:  '))
+name = input('enter the customer name:  ')
+age = int(input('enter the customer age:  '))
+fav_mov = input('enter the customer fav_movies with seperstted comma').split(',')
+
+add_cust(id,name,age,fav_mov)
